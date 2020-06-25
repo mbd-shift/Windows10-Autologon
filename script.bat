e2bf6af00fd4128b2e60c0828414bd777accd777
mbd-shift <67427843+mbd-shift@users.noreply.github.com>
2020-06-25 16:50:00 +0200

    Create script.bat

---
+++ script.bat
@@ -0,0 +1,3 @@
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d ***username***
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d ***password***
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1
